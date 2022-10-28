ef0615aaa6b8667baaab237dc8c3903c2be7de03
Raghunath Koppuravuri <Raghunath.Koppuravuri@concentrix.com>
2022-10-28 11:32:50 +0530

    modified the confidjs

--- config/config.js
+++ config/config.js
@@ -6,11 +6,6 @@ config.web = {};
 config.port = 3000;
 config.hostname = '127.0.0.1';
 config.secret='ilovescotchyscotch';
-config.domain='qa.cnxremindertool.com';
 config.ClientApplicationUrl='*';
-config.dbhost = 'fh-mmsts01';
-config.user = 'mmsdev.ts';
-config.password= 'springweb123!';
-config.database = 'reminder_tool';
 
 module.exports = config;
